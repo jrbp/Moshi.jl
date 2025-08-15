a8d9eadcf45bb51954c222bc7cb4373555eac813
John Bonini <bonini.physics@gmail.com>
2025-08-15 01:53:16 -0400

    ref type check and match pattern supertypes
    attempt at fixing #48

--- src/match/emit/collection/vect.jl
+++ src/match/emit/collection/vect.jl
@@ -8,9 +8,23 @@ function decons(::Type{Pattern.Ref}, ctx::PatternContext, pat::Pattern.Type)
     #    will try to find the index that returns the input
     #    value.
     # 2 is not supported for now because I don't see any use case.
-    return CollectionDecons(ctx, pat, pat.args) do _
-        :($Base.Vector{$(pat.head)})
+    coll = CollectionDecons(ctx, pat, pat.args) do _
+        :($Base.Vector{<:$(pat.head)})
     end
+    set_view_type_check!(coll) do view, eltype
+        # For the pattern
+        #   AP[_, v::VP..., _]
+        # matching on some vector of the form
+        #   A[_, V[...]..., _]
+        # the vector is illegal unless V <: A
+        # and the match fails elsewhere unless A <: AP
+        # so V <: A <: AP
+        # want to return V <: VP
+        (pat.head == eltype # if AP == VP we know at macro time
+         || :(eltype($view) <: $eltype # if A <: VP can avoid iteration
+              || all(Base.Fix2(isa, $eltype), $view)))
+    end
+    return coll
 end
 
 function decons(::Type{Pattern.Vector}, ctx::PatternContext, pat::Pattern.Type)
@@ -18,7 +32,8 @@ function decons(::Type{Pattern.Vector}, ctx::PatternContext, pat::Pattern.Type)
         :($Base.Vector)
     end
     set_view_type_check!(coll) do view, eltype
-        :(eltype($view) == $eltype)
+        :(eltype($view) <: $eltype
+          || all(Base.Fix2(isa, $eltype), $view))
     end
     return coll
 end
